d49bf8ea18cb4ccd215a4fb3c387736c76600677
Наталья <NataliaVlaskina@mail.ru>
2018-07-29 19:33:08 +0300

    Добавлен атрибут key для картинок

--- src/components/header/HeaderLives.js
+++ src/components/header/HeaderLives.js
@@ -2,18 +2,14 @@ import React, {Component} from 'react'
 
 class HeaderLives extends Component {
 	render() {
-		const heartEmpty = new Array(3 - this.props.lives).fill(<img src="img/heart__empty.svg" className="game__heart" alt="Life" width="32" height="32" /> )
+		const heartEmpty = [<img src="img/heart__empty.svg" className="game__heart" alt="Life" width="32" height="32" key="4" /> , <img src="img/heart__empty.svg" className="game__heart" alt="Life" width="32" height="32" key="5" /> , <img src="img/heart__empty.svg" className="game__heart" alt="Life" width="32" height="32" key="6" /> ]
 		
-		const heartFull = new Array(this.props.lives).fill(<img src="img/heart__full.svg" className="game__heart" alt="Life" width="32" height="32" /> )
+		const heartFull = [<img src="img/heart__full.svg" className="game__heart" alt="Life" width="32" height="32" key="1" /> , <img src="img/heart__full.svg" className="game__heart" alt="Life" width="32" height="32" key="2" />, <img src="img/heart__full.svg" className="game__heart" alt="Life" width="32" height="32" key="3" />]
 		
 		return (
 		<div className="game__lives"> 
-			{heartEmpty.length > 0 ? heartEmpty.map( it => {
-				return it
-			}) : ""}
-			{heartFull.length > 0 ? heartFull.map( it => {
-				return it
-			}) : ""}
+			{this.props.lives < 3 ? heartEmpty.slice(0, 3 - this.props.lives) : ""}
+			{heartFull.length > 0 ? heartFull.slice(0, this.props.lives) : ""}
 		</div>	
 		)
 	}
